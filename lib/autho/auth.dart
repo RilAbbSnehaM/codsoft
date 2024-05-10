376d71a5452f51644b08c1b669b49383ce2b2cb6
RilAbb SnehaM <snehamathur012@gmail.com>
2024-05-11 01:41:31 +0530

    codespace

---
+++ lib/autho/auth.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+import 'package:codsoft/screen/login.dart';
+
+import '../screen/signup.dart';
+
+class Auth_Page extends StatefulWidget {
+  Auth_Page({super.key});
+
+  @override
+  State<Auth_Page> createState() => _Auth_PageState();
+}
+
+class _Auth_PageState extends State<Auth_Page> {
+  bool a = true;
+  void to() {
+    setState(() {
+      a = !a;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (a) {
+      return Login(to);
+    } else {
+      return SignUp_Screen(to);
+    }
+  }
+}
